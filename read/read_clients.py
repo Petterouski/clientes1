2d46211300e0a79ccdc6e5acc0c7d37f4aacf831
Petterouski <petterrp2002@hotmail.com>
2025-08-04 13:31:54 -0500

    CI/CD pruebeishion 3

--- read/read_clients.py
+++ read/read_clients.py
@@ -3,8 +3,9 @@ import os
 from flask import Flask, jsonify
 from flask_cors import CORS  # ✅ Importar CORS
 
-# prueba 2 CI/CD
+# prueba 3 CI/CD
 # Configuración de rutas
+#
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(current_dir)
 sys.path.append(project_root)
